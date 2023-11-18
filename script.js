0114eb4a444478cc2660bf353c1e4267c65cbe63
Kongonathan1 <143329639+Kongonathan1@users.noreply.github.com>
2023-11-18 17:20:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const btnToggle = document.querySelector('.toggle-btn')
 const navbar = document.querySelector('.navbar')
-
 btnToggle.addEventListener('click', () => {
     navbar.classList.toggle('open')
+})
+
+
+const ratio = 0.25
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio,
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('.reveal').forEach( r => {
+    observer.observe(r)
 })
